c05683bdb4644e00d50f0639b0bfbbbcce891285
yammmie <yammmie1234@gmail.com>
2023-09-19 19:13:45 +0900

    Chapter08 Example Finished!

---
+++ Chapter08/Cat.java
@@ -0,0 +1,8 @@
+package Chapter08;
+
+public class Cat implements Soundable {
+    @Override
+    public String sound() {
+        return "야옹";
+    }
+}
---
+++ Chapter08/Dog.java
@@ -0,0 +1,8 @@
+package Chapter08;
+
+public class Dog implements Soundable {
+    @Override
+    public String sound() {
+        return "멍멍";
+    }
+}
